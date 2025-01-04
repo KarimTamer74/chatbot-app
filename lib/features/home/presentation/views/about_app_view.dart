15503c01719e20c56afbc4c5f380bd6bf18fbfd6
KarimTamer74 <kemotamer448@gmail.com>
2025-01-04 19:16:07 +0200

    create a basic profile & about app views

---
+++ lib/features/home/presentation/views/about_app_view.dart
@@ -0,0 +1,18 @@
+import 'package:chatbot_app/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class AboutAppView extends StatelessWidget {
+  const AboutAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "About App View",
+          style: Styles.textStyleBold22,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/profile_view.dart
@@ -0,0 +1,18 @@
+import 'package:chatbot_app/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Profile View",
+          style: Styles.textStyleBold22,
+        ),
+      ),
+    );
+  }
+}
