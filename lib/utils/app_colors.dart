4b22303ae8287031aa3d7a2052e9878e45bb7706
KarimTamer74 <kemotamer448@gmail.com>
2025-02-02 18:36:55 +0200

    add more colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -14,7 +14,7 @@ class AppColors {
   static Color kdeepPurple = Colors.deepPurple;
   static Color kdeepPurpleAccent = Colors.deepPurpleAccent;
 
-//* blue colors
+  //* blue colors
   static Color kBlueShade50 = const Color(0xFFE3F2FD);
   static Color kBlueShade100 = const Color(0xFFBBDEFB);
   static Color kBlueShade200 = const Color(0xFF90CAF9);
@@ -25,4 +25,150 @@ class AppColors {
   static Color kBlueShade700 = const Color(0xFF1976D2);
   static Color kBlueShade800 = const Color(0xFF1565C0);
   static Color kBlueShade900 = const Color(0xFF0D47A1);
+
+  //* Brown colors
+  static Color kBrownShade50 = const Color(0xFFEFEBE9);
+  static Color kBrownShade100 = const Color(0xFFD7CCC8);
+  static Color kBrownShade200 = const Color(0xFFBCAAA4);
+  static Color kBrownShade300 = const Color(0xFFA1887F);
+  static Color kBrownShade400 = const Color(0xFF8D6E63);
+  static Color kBrownShade500 = const Color(0xFF795548);
+  static Color kBrownShade600 = const Color(0xFF6D4C41);
+  static Color kBrownShade700 = const Color(0xFF5D4037);
+  static Color kBrownShade800 = const Color(0xFF4E342E);
+  static Color kBrownShade900 = const Color(0xFF3E2723);
+
+  //* Cyan colors
+  static Color kCyanShade50 = const Color(0xFFE0F7FA);
+  static Color kCyanShade100 = const Color(0xFFB2EBF2);
+  static Color kCyanShade200 = const Color(0xFF80DEEA);
+  static Color kCyanShade300 = const Color(0xFF4DD0E1);
+  static Color kCyanShade400 = const Color(0xFF26C6DA);
+  static Color kCyanShade500 = const Color(0xFF00BCD4);
+  static Color kCyanShade600 = const Color(0xFF00ACC1);
+  static Color kCyanShade700 = const Color(0xFF0097A7);
+  static Color kCyanShade800 = const Color(0xFF00838F);
+  static Color kCyanShade900 = const Color(0xFF006064);
+
+  //* Lime colors
+  static Color kLimeShade50 = const Color(0xFFF9FBE7);
+  static Color kLimeShade100 = const Color(0xFFF0F4C3);
+  static Color kLimeShade200 = const Color(0xFFE6EE9C);
+  static Color kLimeShade300 = const Color(0xFFDCE775);
+  static Color kLimeShade400 = const Color(0xFFD4E157);
+  static Color kLimeShade500 = const Color(0xFFCDDC39);
+  static Color kLimeShade600 = const Color(0xFFC0CA33);
+  static Color kLimeShade700 = const Color(0xFFAFB42B);
+  static Color kLimeShade800 = const Color(0xFF9E9D24);
+  static Color kLimeShade900 = const Color(0xFF827717);
+
+  //* Amber colors
+  static Color kAmberShade50 = const Color(0xFFFFF8E1);
+  static Color kAmberShade100 = const Color(0xFFFFECB3);
+  static Color kAmberShade200 = const Color(0xFFFFE082);
+  static Color kAmberShade300 = const Color(0xFFFFD54F);
+  static Color kAmberShade400 = const Color(0xFFFFCA28);
+  static Color kAmberShade500 = const Color(0xFFFFC107);
+  static Color kAmberShade600 = const Color(0xFFFFB300);
+  static Color kAmberShade700 = const Color(0xFFFFA000);
+  static Color kAmberShade800 = const Color(0xFFFF8F00);
+  static Color kAmberShade900 = const Color(0xFFFF6F00);
+
+  //* red colors
+  static Color kRedShade50 = const Color(0xFFFFEBEE);
+  static Color kRedShade100 = const Color(0xFFFFCDD2);
+  static Color kRedShade200 = const Color(0xFFEF9A9A);
+  static Color kRedShade300 = const Color(0xFFE57373);
+  static Color kRedShade400 = const Color(0xFFEF5350);
+  static Color kRedShade500 = const Color(0xFFF44336);
+  static Color kRedShade600 = const Color(0xFFE53935);
+  static Color kRedShade700 = const Color(0xFFD32F2F);
+  static Color kRedShade800 = const Color(0xFFC62828);
+  static Color kRedShade900 = const Color(0xFFB71C1C);
+
+  //* green colors
+  static Color kGreenShade50 = const Color(0xFFE8F5E9);
+  static Color kGreenShade100 = const Color(0xFFC8E6C9);
+  static Color kGreenShade200 = const Color(0xFFA5D6A7);
+  static Color kGreenShade300 = const Color(0xFF81C784);
+  static Color kGreenShade400 = const Color(0xFF66BB6A);
+  static Color kGreenShade500 = const Color(0xFF4CAF50);
+  static Color kGreenShade600 = const Color(0xFF43A047);
+  static Color kGreenShade700 = const Color(0xFF388E3C);
+  static Color kGreenShade800 = const Color(0xFF2E7D32);
+  static Color kGreenShade900 = const Color(0xFF1B5E20);
+
+  //* yellow colors
+  static Color kYellowShade50 = const Color(0xFFFFFDE7);
+  static Color kYellowShade100 = const Color(0xFFFFF9C4);
+  static Color kYellowShade200 = const Color(0xFFFFF59D);
+  static Color kYellowShade300 = const Color(0xFFFFF176);
+  static Color kYellowShade400 = const Color(0xFFFFEE58);
+  static Color kYellowShade500 = const Color(0xFFFFEB3B);
+  static Color kYellowShade600 = const Color(0xFFFDD835);
+  static Color kYellowShade700 = const Color(0xFFFBC02D);
+  static Color kYellowShade800 = const Color(0xFFF9A825);
+  static Color kYellowShade900 = const Color(0xFFF57F17);
+
+  //* orange colors
+  static Color kOrangeShade50 = const Color(0xFFFFF3E0);
+  static Color kOrangeShade100 = const Color(0xFFFFE0B2);
+  static Color kOrangeShade200 = const Color(0xFFFFCC80);
+  static Color kOrangeShade300 = const Color(0xFFFFB74D);
+  static Color kOrangeShade400 = const Color(0xFFFFA726);
+  static Color kOrangeShade500 = const Color(0xFFFF9800);
+  static Color kOrangeShade600 = const Color(0xFFFB8C00);
+  static Color kOrangeShade700 = const Color(0xFFF57C00);
+  static Color kOrangeShade800 = const Color(0xFFEF6C00);
+  static Color kOrangeShade900 = const Color(0xFFE65100);
+
+  //* pink colors
+  static Color kPinkShade50 = const Color(0xFFFCE4EC);
+  static Color kPinkShade100 = const Color(0xFFF8BBD0);
+  static Color kPinkShade200 = const Color(0xFFF48FB1);
+  static Color kPinkShade300 = const Color(0xFFF06292);
+  static Color kPinkShade400 = const Color(0xFFEC407A);
+  static Color kPinkShade500 = const Color(0xFFE91E63);
+  static Color kPinkShade600 = const Color(0xFFD81B60);
+  static Color kPinkShade700 = const Color(0xFFC2185B);
+  static Color kPinkShade800 = const Color(0xFFAD1457);
+  static Color kPinkShade900 = const Color(0xFF880E4F);
+
+  //* purple colors
+  static Color kPurpleShade50 = const Color(0xFFF3E5F5);
+  static Color kPurpleShade100 = const Color(0xFFE1BEE7);
+  static Color kPurpleShade200 = const Color(0xFFCE93D8);
+  static Color kPurpleShade300 = const Color(0xFFBA68C8);
+  static Color kPurpleShade400 = const Color(0xFFAB47BC);
+  static Color kPurpleShade500 = const Color(0xFF9C27B0);
+  static Color kPurpleShade600 = const Color(0xFF8E24AA);
+  static Color kPurpleShade700 = const Color(0xFF7B1FA2);
+  static Color kPurpleShade800 = const Color(0xFF6A1B9A);
+  static Color kPurpleShade900 = const Color(0xFF4A148C);
+
+  //* teal colors
+  static Color kTealShade50 = const Color(0xFFE0F2F1);
+  static Color kTealShade100 = const Color(0xFFB2DFDB);
+  static Color kTealShade200 = const Color(0xFF80CBC4);
+  static Color kTealShade300 = const Color(0xFF4DB6AC);
+  static Color kTealShade400 = const Color(0xFF26A69A);
+  static Color kTealShade500 = const Color(0xFF009688);
+  static Color kTealShade600 = const Color(0xFF00897B);
+  static Color kTealShade700 = const Color(0xFF00796B);
+  static Color kTealShade800 = const Color(0xFF00695C);
+  static Color kTealShade900 = const Color(0xFF004D40);
+
+  //* indigo colors
+  static Color kIndigoShade50 = const Color(0xFFE8EAF6);
+  static Color kIndigoShade100 = const Color(0xFFC5CAE9);
+  static Color kIndigoShade200 = const Color(0xFF9FA8DA);
+  static Color kIndigoShade300 = const Color(0xFF7986CB);
+  static Color kIndigoShade400 = const Color(0xFF5C6BC0);
+  static Color kIndigoShade500 = const Color(0xFF3F51B5);
+  static Color kIndigoShade600 = const Color(0xFF3949AB);
+  static Color kIndigoShade700 = const Color(0xFF303F9F);
+  static Color kIndigoShade800 = const Color(0xFF283593);
+  static Color kIndigoShade900 = const Color(0xFF1A237E);
+
+  
 }
